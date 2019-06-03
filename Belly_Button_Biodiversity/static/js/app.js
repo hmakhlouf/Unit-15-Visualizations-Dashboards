06f124a097088c401b1e72b0d7c5ecaf543c53ad
Hocine Makhlouf <datamakhlouf@gmail.com>
2019-06-03 15:25:50 -0500

    hw15

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -1,29 +1,59 @@
+
+
 function buildMetadata(sample) {
+
+
+
 
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
 
-    // Use `.html("") to clear any existing metadata
+  const url = "/metadata/<sample>";
 
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
+  d3.json(url).then(function(data) {
+    console.log(data);
+  });
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+
+//     // Use d3 to select the panel with id of `#sample-metadata`
+
+//     // Use `.html("") to clear any existing metadata
+
+//     // Use `Object.entries` to add each key and value pair to the panel
+//     // Hint: Inside the loop, you will need to use d3 to append new
+//     // tags for each key-value in the metadata.
+
+//     // BONUS: Build the Gauge Chart
+//     // buildGauge(data.WFREQ);
 }
 
 function buildCharts(sample) {
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+// @TODO: Use `d3.json` to fetch the sample data for the plots
 
-    // @TODO: Build a Bubble Chart using the sample data
+  const url = "/samples/<sample>";
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
+  d3.json(url).then(function(data) {
+    console.log(data);
+  });
+  
+
+//     @TODO: Build a Bubble Chart using the sample data
+
+//     @TODO: Build a Pie Chart
+
+data = [{
+  "labels": sample_data.otu_id.values.tolist(),
+  "values": sample_data[sample].values.tolist(),
+  "hovertext": sample_data.otu_label.tolist(),
+  "type": "pie"}]
+
+var layout = {
+  title: "Pie chart"}
+Plotly.plot("pie", data, layout);
+//     HINT: You will need to use slice() to grab the top 10 sample_values,
+//     otu_ids, and labels (10 each).
 }
 
 function init() {
